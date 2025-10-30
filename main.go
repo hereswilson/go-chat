1dcf11f49b4b8a48c904767b06c7cb4aca860e3b
Wilson Heres <wheres67@gmail.com>
2025-10-30 13:40:47 -0400

    Add Fiber server setup with static file serving and message handling
    - Implemented views engine for HTML templates
    - Added static file serving for CSS
    - Created AppHandler for managing routes
    - Added index and messages HTML templates

--- main.go
+++ main.go
@@ -1,16 +1,32 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+	"github.com/hereswilson/go-chat/internal/handlers"
+)
 
 func main() {
 
+	viewsEngine := html.New("./views", ".html")
+
 	// Start new fiber instance
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		Views: viewsEngine,
+	})
+
+	app.Static("/static/", "./static")
 
 	// Create a "ping" handler to test the server
 	app.Get("/ping", func(ctx *fiber.Ctx) error {
 		return ctx.SendString("Welcome to fiber")
 	})
+
+	// create new App Handler
+	appHandler := handlers.NewAppHandler()
+
+	// Add appHandler routes
+	app.Get("/", appHandler.HandleGetIndex)
 
 	// Start the http server
 	app.Listen(":3000")
